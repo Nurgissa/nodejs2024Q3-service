a6895ad30b77db4d5ea68c6b8cd8c3117c3a0eda
Nurgissa <nurgisa@gmail.com>
2024-11-10 15:56:13 +0100

    feat: create user module with stub methods

---
+++ src/user/user.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+
+interface CreateUserDto {
+  login: string;
+  password: string;
+}
+
+interface UpdatePasswordDto {
+  oldPassword: string; // previous password
+  newPassword: string; // new password
+}
+
+@Controller('user')
+export class UserController {
+  @Get()
+  findAll(): string {
+    return 'findAll';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): any {
+    return `findOne: ${id}`;
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto): string {
+    return `create ${JSON.stringify(createUserDto)}`;
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() upddatePasswordDto: UpdatePasswordDto,
+  ): string {
+    return `update: ${id} - ${JSON.stringify(upddatePasswordDto)}`;
+  }
+}
